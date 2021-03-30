f6c4477a9fed0a6cf5a2bab66f2d6cb120baae58
= <=>
2021-03-30 19:16:46 -0230

    Refactor cra files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.scss";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./sw/serviceWorkerRegistration";
+import reportWebVitals from "./sw/reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
